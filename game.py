d6814ecb389e426debf64e0df8f9901be3db35eb
Daniel Juravski <juravski95@gmail.com>
2020-10-06 11:38:36 +0300

    add some documentation

--- game.py
+++ game.py
@@ -1,21 +1,22 @@
-import sys
 import numpy as np
-from termcolor import colored
-from sty import fg, bg, ef, rs
 from copy import deepcopy
-
 
 M = 18
 N = 18
 COLORS = ['r', 'b', 'g', 'y']
 COLORS_MAP = {'r': '\033[41m', 'b': '\033[44m', 'g': '\033[42m', 'y': '\033[43m'}
 CEND = '\033[0m'
-
 WIN_TURNS = 21
 
 
 class Game:
+    """
+    color flood game
+    """
     def __init__(self):
+        """
+        init game - supporting actions history and knight mode
+        """
         self.board_history = []
         self.board = self.init_board()
         self.board_history.append(deepcopy(self.board))
@@ -24,6 +25,10 @@ class Game:
         self.knight_mode = False
 
     def init_board(self):
+        """
+        create M*N board randomly values with the COLORS chars
+        :return:
+        """
         print("Initializing board at size {}*{}".format(M, N))
         board = np.random.choice(COLORS, size=(M, N))
         print()
@@ -31,20 +36,28 @@ class Game:
         return board
 
     def play(self):
+        """
+        play loop until all the board is one color
+        :return:
+        """
         turn_current = 0
         self.print_board()
 
+        # play until the game is end
         while not self.is_game_end():
             user_input = self.get_user_input(turn_current)
 
             if user_input == 'u':
+                # undo last move (last color spreading)
                 if len(self.board_history) > 1:
                     self.board_history.pop()
                     self.board = deepcopy(self.board_history[-1])
                     turn_current -= 1
             elif user_input == 'k':
+                # set knight mode - the alg. for color spreading is calculated via the knight steps
                 self.knight_mode = not self.knight_mode
             else:
+                # spread colors to all the neighbors
                 self.spread_colors(user_input)
                 turn_current += 1
                 self.board_history.append(deepcopy(self.board))
@@ -54,6 +67,10 @@ class Game:
         self.game_end(turn_current)
 
     def is_game_end(self):
+        """
+        check all matrix values are the same color
+        :return:
+        """
         eq = np.all(self.board == self.board[0][0])
 
         return eq
@@ -65,6 +82,11 @@ class Game:
             print("Game Over, You Lose!")
 
     def get_user_input(self, turn_current):
+        """
+        get char input from user, supports only COLORS values, u value - for undo, k value for knight mode
+        :param turn_current:
+        :return:
+        """
         is_input_valid = False
         while not is_input_valid:
             if self.knight_mode:
@@ -74,6 +96,7 @@ class Game:
             user_input = input(input_msg)
             if user_input in COLORS or user_input == 'u' or user_input == 'k':
                 is_input_valid = True
+                print()
 
         return user_input
 
@@ -85,6 +108,11 @@ class Game:
             print()
 
     def spread_colors(self, new_color):
+        """
+        spread color to all the 0,0 neighbors that had the previous color that 0,0 had
+        :param new_color:
+        :return:
+        """
         cands_approved = set()
         cands_approved.add((0, 0))
         cands_declined = set()
@@ -94,20 +122,34 @@ class Game:
             self.board[i][j] = new_color
 
     def get_neighbors_to_color(self, i, j, cands_approved, cands_declined):
+        """
+        find all neighbors of i,j cell (by the desired directions mode) that have the same color as it has
+        :param i: src cell i value
+        :param j: src cell j value
+        :param cands_approved: cells that already were checked and approved as neighbors to be colored - shouldn't be checked again
+        :param cands_declined: cells that already were checked and declined as neighbors to be colored - shouldn't be checked again
+        :return: cands_approved, cands_declined
+        """
+        # set the desired directions to search on
         directions = self.possible_directions if self.knight_mode == False else self.knight_directions
         for cand_direction in directions:
             cand_i = i + cand_direction[0]
             cand_j = j + cand_direction[1]
 
+            # if the cand cell in one of the bellow list - do not search it
             if (cand_i, cand_j) in cands_approved or (cand_i, cand_j) in cands_declined:
                 continue
 
+            # try to access some i,j in the board matrix
             try:
+                # cand i,j can be negative due to some directions - ignore them
                 if cand_i < 0 or cand_j < 0:
                     continue
+                # check if cand cell color as the src cell color
                 if self.board[cand_i][cand_j] == self.board[i][j]:
                     cands_approved.add((cand_i, cand_j))
 
+                    # search all neighbors of the accepted neighbor
                     partial_cands_approved, partial_cands_declined = self.get_neighbors_to_color(cand_i, cand_j,
                                                                                                  cands_approved,
                                                                                                  cands_declined)
@@ -116,6 +158,7 @@ class Game:
                 else:
                     cands_declined.add((cand_i, cand_j))
 
+            # that try should except on IndexError values
             except IndexError as e:
                 continue
 
